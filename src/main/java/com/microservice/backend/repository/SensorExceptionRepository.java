eb9a0f99797ccb881f41ebe7db67f3ff4dead76b
Yinvoker <Yinvoker@outlook.com>
2018-12-14 18:04:09 +0800

    bug修复

--- src/main/java/com/microservice/backend/repository/SensorExceptionRepository.java
+++ src/main/java/com/microservice/backend/repository/SensorExceptionRepository.java
@@ -4,6 +4,7 @@ import com.microservice.backend.entity.SensorException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface SensorExceptionRepository extends JpaRepository<SensorException,Long> {
@@ -12,5 +13,5 @@ public interface SensorExceptionRepository extends JpaRepository<SensorException
     List<SensorException> findAll();
 
     @Query(value = "SELECT sensor_exception from SensorException sensor_exception where sensor_exception.status=0 and sensor_exception.time>?1 and sensor_exception.time<?2",nativeQuery = true)
-    List<SensorException> findByTime(String dataFrom, String dataTo);
+    List<SensorException> findByTime(Date dataFrom, Date dataTo);
 }
